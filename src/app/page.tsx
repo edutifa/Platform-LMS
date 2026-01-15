86f38bb4dd000b66f1d947dfb932f4bfb08edad6
nan022 <ronanda20ti@mahasiswa.pcr.ac.id>
2026-01-15 16:43:34 +0700

    Navbar & hero section adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/implements/header";
+import { CarouselPlugin } from "@/components/implements/teknologi-carousel";
 import { LandingHero } from "@/features/landing/components/LandingHero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <Header />
       <LandingHero />
+      <CarouselPlugin />
     </div>
   );
 }
